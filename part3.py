fa533a236fd92f0f3df16487985fc6bc3a9f6dac
wiesliam <wiesliam@umich.edu>
2018-08-17 12:28:27 -0400

    starting beautifulsoup

--- part3.py
+++ part3.py
@@ -1,3 +1,4 @@
+# wiesliam
 # these should be the only imports you need
 
 import requests
@@ -6,3 +7,6 @@ from bs4 import BeautifulSoup
 # write your code here
 # usage should be python3 part3.py
 
+page = requests.get("http://michigandaily.com")
+print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
